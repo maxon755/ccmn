108c654718e56444d0fde0b00b77ef291156842c
Maksim Gayduk <maksim.gayduk@gmail.com>
2020-01-21 20:29:39 +0200

    Initial commit

---
+++ source/components/app/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+
+    return ('APP WORKS');
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './source/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new ErrorOverlayPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    }
+                ]
+            },
+        ]
+    },
+};
