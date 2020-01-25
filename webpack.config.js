026022b6ac7fbe92a69cdde0ba8c44d4d801a551
Maksim Gayduk <maksim.gayduk@gmail.com>
2020-01-25 11:01:15 +0200

    Added .env support

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,58 @@
 const webpack = require('webpack');
 const path = require('path');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-    mode: 'development',
-    entry: './source/index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new ErrorOverlayPlugin()
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 3000,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ],
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties'
-                        ]
+module.exports = (env, argv) => {
+
+    return {
+        entry: './source/index.jsx',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+        },
+        devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+        plugins: [
+            new ErrorOverlayPlugin(),
+            new Dotenv(),
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            port: 3000,
+            historyApiFallback: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react'
+                            ],
+                            plugins: [
+                                '@babel/plugin-proposal-class-properties'
+                            ]
+                        }
                     }
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    }
-                ]
-            },
-        ]
-    },
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        },
+                        {
+                            loader: 'css-loader',
+                        }
+                    ]
+                },
+            ],
+        },
+    }
 };
